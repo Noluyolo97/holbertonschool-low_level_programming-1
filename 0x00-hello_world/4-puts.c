8832802f0e0a5b57ad852745afce74688b530093
SilindaM <silindanemdu@gmail.com>
2022-02-03 10:34:22 +0200

    add 6 size

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- Is the entry point
+ *
+ *
+ *Return 0 to stop the program
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
