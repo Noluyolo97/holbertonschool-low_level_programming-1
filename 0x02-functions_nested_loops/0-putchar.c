a697067d7b092f5f67a0d1a2886dcda0f3417857
SilindaM <silindanemdu@gmail.com>
2022-02-08 10:08:31 +0200

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5, or is zero..
+(*
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return(0);
+}
+
